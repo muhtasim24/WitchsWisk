3483a132ee5cd655598c6afd5511e1024a5d5c66
Muhtasim Mushfiq <muhtasim01@gmail.com>
2026-02-10 16:46:21 -0500

    implemented failed searched queries

--- witchswisk/components/searchProduct.tsx
+++ witchswisk/components/searchProduct.tsx
@@ -12,15 +12,20 @@ type Props = {
 export default function SearchProduct({ products } : Props) {
     const [searchQuery, setSearchQuery] = useState("");
     
-    const filteredProducts = products.filter((product) => product.name.toLowerCase().includes(searchQuery.toLowerCase()));
+    const filteredProducts = products.filter( (product) => (
+        product.name.toLowerCase().includes(searchQuery.toLowerCase())
+    ));
+    
     return (
         <div>
             <input type="search" placeholder="Search Cookie..." value={searchQuery} onChange={ (e) => setSearchQuery(e.target.value)}></input>
             <div className="flex gap-3 grid grid-cols-3 ">
-
-                {filteredProducts.map(product => (
+                {filteredProducts.length === 0 ? (
+                    <p>No cookies found</p>
+                ) : (
+                filteredProducts.map(product => (
                     <Card key={product.id} product={product} />
-                ))}
+                )))}
             </div>
         </div>
 
