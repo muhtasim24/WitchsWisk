4d10fc00764d29122c9cd541ee15f865ae808155
Muhtasim Mushfiq <muhtasim01@gmail.com>
2026-03-12 14:38:33 -0400

    created ugly checkout box in the cart page that show price and total price calculations

--- witchswisk/app/cart/page.tsx
+++ witchswisk/app/cart/page.tsx
@@ -1,3 +1,4 @@
+import CartCheckout from "@/components/cartCheckout";
 import CartView from "@/components/cartView";
 import { getProducts } from "@/lib/getProducts";
 
@@ -8,7 +9,15 @@ export default async function Cart() {
     return (
         <div>
             <h1 className="text-5xl font-bold">Shopping Cart</h1>
-            <CartView products = { products } />
+            <div className="flex gap-50">
+                <div>
+                    <CartView products = { products } />
+                </div>
+       
+                <div>
+                    <CartCheckout products = { products } />
+                </div>
+            </div>
         </div>
     )
 }
---
+++ witchswisk/components/cartCheckout.tsx
@@ -0,0 +1,42 @@
+'use client'
+import { useCart } from "@/app/context/cartContext";
+import { Product } from "@/data/products";
+
+
+type Props = {
+    products: Product[];
+}
+
+export default function CartCheckout( {products} : Props) {
+
+    const { cartItems } = useCart();
+
+    // I want to display name, quantity and price here and total price
+
+    let totalPrice = 0;
+
+
+    return (
+        <div className="bg-brand w-100 h-150 rounded-xl flex flex-col">
+            <h1 className="text-3xl font-bold">Checkout</h1>
+            {cartItems.map(item => {
+                const product = products.find(product => product.id === item.id);
+
+                if (product) {
+                    const price = item.quantity * product.price
+                    totalPrice = totalPrice + price
+                    return (
+                        <div className="flex gap-15">
+                            <h1> {product.name} </h1>
+                            <h1> {item.quantity}x </h1>
+                            <h1> ${price}.00</h1>
+                        </div>
+                    )
+                }
+            })}
+
+            <h1>Total Price: ${totalPrice}.00</h1>
+            <button>PLACE ORDER</button>
+        </div>
+    )
+}
