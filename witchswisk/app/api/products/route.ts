5b54f02e1209de729c0d6ff491448ce6c2757d54
Muhtasim Mushfiq <muhtasim01@gmail.com>
2026-04-07 22:41:06 -0400

    fixing synxtax and json errors in api/product/route file

--- witchswisk/app/api/products/route.ts
+++ witchswisk/app/api/products/route.ts
@@ -1,7 +1,11 @@
 import { getProducts } from "@/lib/getProducts";
+import { NextResponse } from "next/server";
 
 
 export async function GET() {
     const products = await getProducts();
-    return Response.json(products) // before sending the products, we send data as json
+    return new Response(JSON.stringify(products), {
+        status: 200,
+        headers: {"Content-Type": "application/json"},
+    });
 }
