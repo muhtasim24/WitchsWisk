b5b2c8cacd511c36174aebdf7add0f036c27092c
Muhtasim Mushfiq <muhtasim01@gmail.com>
2026-04-07 22:36:59 -0400

    comments in lib/getProducts page

--- witchswisk/lib/getProducts.ts
+++ witchswisk/lib/getProducts.ts
@@ -1,5 +1,8 @@
 import { products } from "@/data/products";
 
+// THIS PAGE CHANGES WHEN IMPLEMENT REAL DB
+// USED TO ACCESS THE DATA, will change when where we access data from is different
+
 export async function getProducts() {
     return products;
 }
