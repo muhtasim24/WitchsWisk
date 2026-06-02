208bc8496a1f7cde5a128ca448b6d6d60cbd795b
Muhtasim Mushfiq <muhtasim01@gmail.com>
2026-06-02 12:56:37 -0400

    retrieving products from supabase db, setup nextjs config for supabase storage for products images

--- witchswisk/lib/getProducts.ts
+++ witchswisk/lib/getProducts.ts
@@ -1,8 +1,15 @@
-import { products } from "@/data/products";
+import { supabase } from "./supabaseClient";
 
 // THIS PAGE CHANGES WHEN IMPLEMENT REAL DB
 // USED TO ACCESS THE DATA, will change when where we access data from is different
 
 export async function getProducts() {
-    return products;
+   const { data, error } = await supabase.from("products").select("*");
+
+   if (error) {
+    console.error(error);
+    return [];
+   }
+
+   return data;
 }
