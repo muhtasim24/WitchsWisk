c8226ae64da91204be5fc735a9d8633d0943cf87
Muhtasim Mushfiq <muhtasim01@gmail.com>
2026-07-23 17:15:51 -0400

    added shipping info to cart checkout page

--- witchswisk/components/cart/cartCheckout.tsx
+++ witchswisk/components/cart/cartCheckout.tsx
@@ -3,6 +3,7 @@ import { useCart } from "@/app/context/cartContext";
 import { supabase } from "@/lib/supabase/client";
 import type { Product } from "@/lib/types";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 
 type Props = {
@@ -13,6 +14,10 @@ export default function CartCheckout( {products} : Props) {
 
     const { cartItems, loadCart } = useCart();
     const router = useRouter();
+    const [fullName, setFullName] = useState("");
+    const [streetAddress, setStreetAddress] = useState("");
+    const [city, setCity] = useState("");
+    const [zipCode, setZipCode] = useState("");
 
     // I want to display name, quantity and price here and total price
 
@@ -57,14 +62,14 @@ export default function CartCheckout( {products} : Props) {
 
 
     return (
-        <div>
+        <div className="flex flex-col">
             {cartItems.map(item => {
                 const product = products.find(product => product.id === item.product_id);
 
                 if (!product) return null;
               
                 return (
-                    <div key = {item.product_id} className="flex gap-15">
+                    <div key = {item.product_id} className="flex gap-30 text-lg">
                         <h1> {product.name} </h1>
                         <h1> {item.quantity}x </h1>
                         <h1> ${item.quantity * product.price}.00</h1>
@@ -72,8 +77,22 @@ export default function CartCheckout( {products} : Props) {
                 );
             })}
 
-            <h1>Total Price: ${totalPrice}.00</h1>
-            <button onClick={handleCheckout}>CHECKOUT</button>
+            <h1>Shipping Info</h1>
+            <form>
+                <label>Full Name</label>
+                <input className="w-full bg-purple-400 rounded-md h-8 text-black border border-black px-2" type="text" value={fullName} onChange={ (e) => setFullName(e.target.value)}></input>
+                
+                <label>Address:</label>
+                <input className="w-full bg-purple-400 rounded-md h-8 text-black border border-black px-2" type="text" value={streetAddress} onChange={ (e) => setStreetAddress(e.target.value)}></input>
+            
+                <label>City:</label>
+                <input className="w-1/4 bg-purple-400 rounded-md h-8 text-black border border-black px-2" type="text" value={streetAddress} onChange={ (e) => setStreetAddress(e.target.value)}></input>
+
+                <label>Zip Code:</label>
+                <input className="w-1/4 bg-purple-400 rounded-md h-8 text-black border border-black px-2" type="text" value={zipCode} onChange={ (e) => setZipCode(e.target.value)}></input>
+            </form>
+            <h1 className="text-xl font-semibold">Total Price: ${totalPrice}.00</h1>
+            <button onClick={handleCheckout} className="px-6 py-2 rounded-lg font-semibold transition-all active:scale-95 bg-white text-brand items-center">PLACE ORDER</button>
         </div>
     )
 }
